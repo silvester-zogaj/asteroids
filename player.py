06f650e900cc3abe078e2110d17f1672dd2fc637
silvester-zogaj <silvester_9x@hotmail.co.uk>
2025-04-02 13:10:23 +0100

    create player rotation

--- player.py
+++ player.py
@@ -10,6 +10,17 @@ class Player(CircleShape):
     def draw(self, screen):     
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -17,3 +28,5 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+    
