dc01e7802b68b3f0681ff95c316fdacd5f89882d
silvester-zogaj <silvester_9x@hotmail.co.uk>
2025-04-02 10:13:27 +0100

    create triangle player

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-from constants import *
 import pygame
+from constants import *
+from player import Player
 
 
 
 def main():
-
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+        
+
+
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
